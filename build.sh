adc33797737d789ed2aefe8c721257e9f878779a
jairoo-dev <jairoalessandermartinez@gmail.com>
2026-05-31 23:36:59 -0600

    Agregar creación automática de superusuario en build

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ set -o errexit
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@edgecom.mx', 'Admin123!')
+"
